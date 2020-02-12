42b359aad0a8359c26f7b137dd1a642bd7e73346
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-12 00:28:36 +0000

    Create app.js

---
+++ client/app.js
@@ -0,0 +1,20 @@
+//ref to login form
+const loginForm = document.getElementById('welcome-form');
+
+//ref to messages section
+const messagesSection = document.getElementById('messages-section');
+
+//ref to messages list
+const messagesList = document.getElementById('messages-list');
+
+//ref to add message form
+const addMessageForm = document.getElementById('add-messages-form'); 
+
+//ref to user name input
+const userNameInput = document.getElementById('username');
+
+//ref to message input
+const messageContentInput = document.getElementById('message-content');
+
+//global variable - user login
+let userName = '';
